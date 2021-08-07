34c30a9ab40a6af28cc230e2d45d5fc9786166f6
Vincent Marklynn <vincent.marklynn@gmail.com>
2021-08-07 16:56:35 -0700

    Added operate() function in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,26 @@ function divide(a,b)
 {
   return a/b;
 }
+
+function operate(a, b, operatorSymbol)
+{
+  switch(operatorSymbol) {
+    case '+':
+      return add(a,b);
+    case '-':
+      return substract(a,b);
+    case '*':
+      return multiply(a,b);
+    case '/':
+      if(b === 0)
+      {
+        throw new Error('Cannot divide by 0.');
+      }
+      else{
+        return divide(a,b);
+      }
+    default:
+      return null;
+  }
+  
+}
