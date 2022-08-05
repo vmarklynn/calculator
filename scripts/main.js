13e5d2f44043bab278ac4ede8564339c5f911397
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-08-04 22:52:47 -0700

    Functioning buttons and display; changes in index.html, main.js, style.css

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,78 @@
-function add(a,b)
-{
+function add(a, b) {
   return a + b;
 }
 
-function substract(a,b)
-{
-   return a - b;
+function substract(a, b) {
+  return a - b;
 }
 
-function multiply(a,b)
-{
+function multiply(a, b) {
   return a * b;
 }
 
-function divide(a,b)
-{
-  return a/b;
+function divide(a, b) {
+  return a / b;
 }
 
-function operate(a, b, operatorSymbol)
-{
-  switch(operatorSymbol) {
+function operate(a, b, operatorSymbol) {
+  switch (operatorSymbol) {
     case '+':
-      return add(a,b);
+      return add(a, b);
     case '-':
-      return substract(a,b);
+      return substract(a, b);
     case '*':
-      return multiply(a,b);
+      return multiply(a, b);
     case '/':
-      if(b === 0)
-      {
+      if (b === 0) {
         throw new Error('Cannot divide by 0.');
       }
-      else{
-        return divide(a,b);
+      else {
+        return divide(a, b);
       }
     default:
       return null;
   }
-  
+
 }
+
+const container = document.querySelector(".container");
+const digits = container.querySelector(".digits");
+const nums = document.querySelectorAll("#num");
+const operators = document.querySelectorAll("#operator");
+const resets = document.querySelectorAll("#reset");
+const decimal = document.querySelector("#decimal");
+const equal = document.querySelector("#equal");
+
+let curValue = "";
+let secondaryValue = "";
+let curOperator = "";
+let len = 0;
+let decimalCount = 0;
+let result = 0;
+
+
+
+nums.forEach(num => num.addEventListener('click', () => {
+  if (curValue.length < 9) {
+    secondaryValue = num.textContent.trim();
+    if (curValue[0] != "0" || curValue == "") {
+      if (curOperator.length === 1) {
+        digits.textContent = secondaryValue;
+        curOperator = "";
+        curValue = result.toString();
+
+      }
+      else {
+        curValue += secondaryValue;
+        secondaryValue = "";
+      }
+      digits.textContent = curValue;
+
+    }
+    else {
+      return;
+    }
+  }
+}));
+
+
